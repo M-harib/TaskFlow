d6cc1b3438b28e7f9e04628046bddb2071c10c57
M-harib <mharib@my.yorku.ca>
2025-10-13 00:34:37 -0400

    fixed error

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,7 @@ from flask_cors import CORS
 from datetime import datetime
 import bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from your_app import db, app
-from backend import db, app
+from app import db, app
 
 with app.app_context():
     db.create_all()
